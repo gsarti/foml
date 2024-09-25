8ddf4356f9e7ef28df4c6a74dcfacfd367d4c465
Gabriele Sarti <gabriele.sarti996@gmail.com>
2024-09-25 19:08:51 +0200

    Update feature_extractor.py

--- feature_extractor.py
+++ feature_extractor.py
@@ -70,7 +70,7 @@ def read_features_from_csv(args: argparse.Namespace) -> Tuple[List, np.ndarray]:
 
 
 def get_column_types(header: List[str]) -> np.ndarray:
-    types: np.ndarray = np.zeros((len(header),), type=object)
+    types: np.ndarray = np.zeros((len(header),), dtype=object)
 
     for idx, name in enumerate(header):
         if "cat" in name:
